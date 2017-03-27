125ae0eddf4494298ae2328860647dd48aaa5acb
maumgadk <maumgadk@gmail.com>
2017-03-27 20:28:22 +0900

    Add init.bat

---
+++ init.bat
@@ -0,0 +1,3 @@
+echo 0 > iter.txt
+del model.ckpt*
+del psnr.log
--- test.py
+++ test.py
@@ -460,6 +460,7 @@ def trainModel(NNlayer, img_data, img_shape, gpu_list, isTest=False):
         PSNR = 0.
 
         start_time = datetime.datetime.now()
+        print('Start time: %s'%str(start_time))
 
         for niter in range(nEpoch):
             if niter >0: rd_idx = 0
